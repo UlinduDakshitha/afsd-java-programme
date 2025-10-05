789cc19a9e5539aaecb36c6ef11a2be5ad88ceea
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-10-05 09:34:04 +0530

    Add updates

--- src/Abcd.java
+++ src/Abcd.java
@@ -12,16 +12,14 @@ public class Abcd {
             System.out.print("Enter your salary:");
             salary[i]=sc.nextDouble();
         }
-        for(int i=0; i<8; i++ )
-        if(salary[i]>130000){
-            newSalaries[i]=salary[i]+(salary[i]*0.12);
+        for(int i=0; i<8; i++ ) {
+            if (salary[i] > 130000) {
+                newSalaries[i] = salary[i] + (salary[i] * 0.12);
+            } else {
+                newSalaries[i] = salary[i];
+            }
+            totalOvercost += (newSalaries[i] - salary[i]);
         }
-        else{
-            newSalaries[i]=salary[i];
-        }
-        totalOvercost += (newSalaries[i]-salary[i]);
-
-
         for (int i=0; i<8; i++){
 
             System.out.println("employee"+(i+1)+newSalaries[i]);
