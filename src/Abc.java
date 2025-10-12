d70b7eed11bd4b48b659d6a38c9819fc38bc361c
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-10-12 08:55:54 +0530

    update

--- src/Abc.java
+++ src/Abc.java
@@ -1,24 +1,35 @@
 import java.util.Scanner;
+
 public class Abc {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.next();
 
-        System.out.print("Enter your name:");
-        String name= sc.next();
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
 
-        System.out.print("Enter your age:");
-        int age=sc.nextInt();
-
-        while(age>100){
-            System.out.print("Enter your correct age:");
-            age=sc.nextInt();
+        while(age > 100) {
+            System.out.print("Enter your correct age: ");
+            age = sc.nextInt();
         }
 
-        System.out.print("Enter your address:");
-        String address= sc.next();
+        // Consume the leftover newline character
+        sc.nextLine();
 
-        System.out.print("Enter your telephonenumber:");
-        int phoneNumber=sc.nextInt();
+        System.out.print("Enter your address: ");
+        String address = sc.nextLine(); // Use nextLine() for multi-word input
+
+        System.out.print("Enter your telephone number: ");
+        int phoneNumber = sc.nextInt();
+
+        // Display the collected information
+        System.out.println("\n--- User Information ---");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+        System.out.println("Telephone: " + phoneNumber);
+
     }
 }
---
+++ src/Methods.java
@@ -0,0 +1,10 @@
+public class Methods {
+    public static void main(String[] args) {
+
+        getTotal();
+    }
+
+    public static void getTotal(){
+        System.out.println("Hello Yasindu");
+    }
+}
